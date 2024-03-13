cf2f03f7b1cec7bb48cce522fc78a99fe88b76a9
Yesid <yesidrodriguez305@gmail.com>
2024-03-13 08:55:31 -0500

    Add initial structure

---
+++ src/main/java/project/dreamyevent/DreamyEventBackApplication.java
@@ -0,0 +1,13 @@
+package project.dreamyevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamyEventBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DreamyEventBackApplication.class, args);
+  }
+
+}
